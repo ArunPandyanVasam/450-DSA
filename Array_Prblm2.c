f3e1cee89297c55047376f2b3ae0e2f4617765a2
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-01-14 17:54:41 -0500

    Array_Prblm2

---
+++ Array_Prblm2.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+
+int main() {
+    //  Find the maximum and minimum element in an array
+    puts("\n");
+
+    int arr[] = {1, 2, 9, 4, 0};
+    int max = arr[0], min = arr[0];
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    printf("Min - %d", min);
+    puts(" ");
+    printf("Max - %d", max);
+    
+
+
+
+
+
+
+
+
+    
+    puts("\n");
+    return 0;
+}
